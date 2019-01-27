06e854257be472d0ca764ac7cb43a8fee3d8dee2
xiebei <xiebei@bytedance.com>
2019-01-27 11:26:52 +0800

    first step of rust
    Change-Id: I51757740bf1ba7a14f0ce7ae7dc6f27585b88aa7

---
+++ rust_code/jump-game-ii/src/main.rs
@@ -0,0 +1,17 @@
+// impl Solution {
+    pub fn jump(nums: Vec<i32>) -> i32 {
+        return "hello world"
+    }
+// }
+
+#[test]
+fn test_jump() {
+    let nums = vec![2,3,1,1,4];
+    assert_eq!(jump(nums), 2);
+}
+
+fn main() {
+    
+}
+
+
